2553a6844c3328e0a39bae75a96d8a3db2c9f54a
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2023-11-29 21:18:04 -0500

    fix boneheaded bugs in map2tod

--- src_lib/map2tod.cu
+++ src_lib/map2tod.cu
@@ -1,6 +1,9 @@
 #include "../include/gpu_mm.hpp"
-#include <cassert>
 
+#include <cassert>
+#include <iostream>
+
+using namespace std;
 using namespace gputils;
 
 namespace gpu_mm {
@@ -29,16 +32,13 @@ static void _check_map2tod_args(float *tod, const float *map, const float *xpoin
 
 static void _check_map2tod_args(Array<float> &tod, const Array<float> &map, const Array<float> &xpointing)
 {
-    assert(tod.on_gpu());
     assert(tod.ndim == 2);
     assert(tod.is_fully_contiguous());
     
-    assert(map.on_gpu());
     assert(map.ndim == 3);
     assert(map.shape[0] == 3);
     assert(map.is_fully_contiguous());
     
-    assert(xpointing.on_gpu());
     assert(xpointing.ndim == 3);
     assert(xpointing.shape[0] == 3);
     assert(xpointing.shape[1] == tod.shape[0]);
@@ -51,7 +51,8 @@ void reference_map2tod(float *tod, const float *map, const float *xpointing, int
 {
     _check_map2tod_args(tod, map, xpointing, ndet, nt, ndec, nra);
     
-    long ns = long(ndet) * long(ndec);
+    long ns = long(ndet) * long(nt);
+    long npix = long(ndec) * long(nra);
 
     for (long s = 0; s < ns; s++) {
 	float px_dec = xpointing[s];
@@ -65,9 +66,9 @@ void reference_map2tod(float *tod, const float *map, const float *xpointing, int
 	float ddec = px_dec - float(idec);
 	float dra = px_ra - float(ira);
 	
-	assert(idec > 0);
+	assert(idec >= 0);
 	assert(idec < ndec-1);
-	assert(ira > 0);
+	assert(ira >= 0);
 	assert(ira < nra-1);
 	
 	long ipix = idec*nra + ira;
@@ -75,23 +76,23 @@ void reference_map2tod(float *tod, const float *map, const float *xpointing, int
 	
 	float w = (1.0-ddec) * (1.0-dra);
 	out += w * map[ipix];
-	out += w * cos_2a * map[ipix+ns];
-	out += w * sin_2a * map[ipix+2*ns];
+	out += w * cos_2a * map[ipix+npix];
+	out += w * sin_2a * map[ipix+2*npix];
 
 	w = (1.0-ddec) * (dra);
 	out += w * map[ipix + 1];
-	out += w * cos_2a * map[ipix+ns + 1];
-	out += w * sin_2a * map[ipix+2*ns + 1];
+	out += w * cos_2a * map[ipix+npix + 1];
+	out += w * sin_2a * map[ipix+2*npix + 1];
 	
 	w = ddec * (1.0 - dra);
 	out += w * map[ipix + nra];
-	out += w * cos_2a * map[ipix+ns + nra];
-	out += w * sin_2a * map[ipix+2*ns + nra];
+	out += w * cos_2a * map[ipix+npix + nra];
+	out += w * sin_2a * map[ipix+2*npix + nra];
 	
 	w = ddec * dra;
 	out += w * map[ipix + nra+1];
-	out += w * cos_2a * map[ipix+ns + nra+1];
-	out += w * sin_2a * map[ipix+2*ns + nra+1];
+	out += w * cos_2a * map[ipix+npix + nra+1];
+	out += w * sin_2a * map[ipix+2*npix + nra+1];
 
 	tod[s] = out;
     }
@@ -100,6 +101,10 @@ void reference_map2tod(float *tod, const float *map, const float *xpointing, int
 
 void reference_map2tod(Array<float> &tod, const Array<float> &map, const Array<float> &xpointing)
 {
+    assert(tod.on_host());
+    assert(map.on_host());
+    assert(xpointing.on_host());
+    
     _check_map2tod_args(tod, map, xpointing);
     
     reference_map2tod(tod.data, map.data, xpointing.data,
@@ -110,20 +115,18 @@ void reference_map2tod(Array<float> &tod, const Array<float> &map, const Array<f
 // -------------------------------------------------------------------------------------------------
 
 
-// Number of blocks should be: 
-//   ceil(ndet * nt / nt_per_block)
-
 __global__ void map2tod_kernel(float *tod, const float *map, const float *xpointing,
 			       int ndet, int nt, int ndec, int nra, int nt_per_block)
 {
-    long s0 = long(blockIdx.x) * long(nt_per_block);
+    // Number of blocks should be: ceil(ns / nt_per_block)
     long ns = long(ndet) * long(nt);
-    
-    tod += s0;
-    map += s0;
-    xpointing += s0;
+    long s0 = long(blockIdx.x) * long(nt_per_block);
 
-    int n = min(ns - s0, long(nt_per_block));
+    tod += s0;
+    xpointing += s0;
+    
+    int n = min(ns-s0, long(nt_per_block));
+    int npix = ndec * nra;
     
     for (int i = threadIdx.x; i < n; i += blockDim.x) {
 	float px_dec = xpointing[i];
@@ -134,6 +137,12 @@ __global__ void map2tod_kernel(float *tod, const float *map, const float *xpoint
 
 	int idec = int(px_dec);
 	int ira = int(px_ra);
+
+	// assert(idec >= 0);
+	// assert(idec < ndec-1);
+	// assert(ira >= 0);
+	// assert(ira < nra-1);
+	    
 	int ipix = idec*nra + ira;
 	float ddec = px_dec - float(idec);
 	float dra = px_ra - float(ira);
@@ -141,23 +150,23 @@ __global__ void map2tod_kernel(float *tod, const float *map, const float *xpoint
 
 	float w = (1.0-ddec) * (1.0-dra);
 	out += w * map[ipix];
-	out += w * cos_2a * map[ipix+ns];
-	out += w * sin_2a * map[ipix+2*ns];
+	out += w * cos_2a * map[ipix+npix];
+	out += w * sin_2a * map[ipix+2*npix];
 
 	w = (1.0-ddec) * (dra);
 	out += w * map[ipix + 1];
-	out += w * cos_2a * map[ipix+ns + 1];
-	out += w * sin_2a * map[ipix+2*ns + 1];
+	out += w * cos_2a * map[ipix+npix + 1];
+	out += w * sin_2a * map[ipix+2*npix + 1];
 	
 	w = ddec * (1.0 - dra);
 	out += w * map[ipix + nra];
-	out += w * cos_2a * map[ipix+ns + nra];
-	out += w * sin_2a * map[ipix+2*ns + nra];
+	out += w * cos_2a * map[ipix+npix + nra];
+	out += w * sin_2a * map[ipix+2*npix + nra];
 	
 	w = ddec * dra;
 	out += w * map[ipix + nra+1];
-	out += w * cos_2a * map[ipix+ns + nra+1];
-	out += w * sin_2a * map[ipix+2*ns + nra+1];
+	out += w * cos_2a * map[ipix+npix + nra+1];
+	out += w * sin_2a * map[ipix+2*npix + nra+1];
 
 	tod[i] = out;
     }
@@ -184,9 +193,13 @@ void launch_map2tod(float *tod, const float *map, const float *xpointing,
 }
 
 
-void launch_map2tod(Array<float> &tod, Array<float> &map, Array<float> &xpointing,
+void launch_map2tod(Array<float> &tod, const Array<float> &map, const Array<float> &xpointing,
 		    cudaStream_t stream, int nthreads_per_block, int nt_per_block)
 {
+    assert(tod.on_gpu());
+    assert(map.on_gpu());
+    assert(xpointing.on_gpu());
+	
     _check_map2tod_args(tod, map, xpointing);
     
     launch_map2tod(tod.data, map.data, xpointing.data,
